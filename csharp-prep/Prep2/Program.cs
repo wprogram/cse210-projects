af5c2dec8ccbe0fb2fc706d2253c8359dbcaedbe
Peace Etaje <peace.ejiroetaje@gmail.com>
2024-10-06 04:06:49 -0600

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage: ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        string sign =  "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter != "A" && letter != "F")
+        {
+            int lastDigit = percent % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A")
+        {
+
+           int lastDigit = percent % 10;
+            if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        } 
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
-}
+   }
