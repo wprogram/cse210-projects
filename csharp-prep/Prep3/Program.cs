fcc1c387379ac5f4ccb0c1dfa233b7c751bf472b
Peace Etaje <peace.ejiroetaje@gmail.com>
2024-10-06 05:01:09 -0600

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+    {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.WriteLine($"The magic number is: {magicNumber}");
+
+        int guess = -1; 
+        int guessCount = 0;
+
+        Console.WriteLine("I have picked a magic number between 1 and 100. Try to guess it!");
+
+        while (guess != magicNumber)
+        {
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+        guess = int.Parse(guessInput);
+        guessCount++;
+
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine($"You guessed it! It took you {guessCount} guesses.");
+        }
+    }
+        Console.Write("Do you want to play again? (yes/no): ");
+        playAgain = Console.ReadLine();
+    }
+     Console.WriteLine("Thanks for playing!");
     }
 }
